4ef9f530685cada7b837957274a4e6dd761bd5d1
Woohyun Park <87453250+Mickey777777@users.noreply.github.com>
2025-11-21 18:29:58 +0900

    feat: 2차원 배열을 이용해서 그리드 그리기 (#2)

---
+++ src/BoardPanel.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardPanel extends JPanel {
+    private static final int TILE_SIZE = 100;
+    private static final int MARGIN = 10;
+
+    private GameBoard board;
+
+    public BoardPanel(GameBoard board){
+        this.board = board;
+        int GRID_SIZE = board.getSize();
+        int panelSize = GRID_SIZE * (TILE_SIZE + MARGIN) + MARGIN;
+        setPreferredSize(new Dimension(panelSize, panelSize));
+        setBackground(new Color(153, 139, 124));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        drawGrid(g2d);
+    }
+
+    private void drawGrid(Graphics2D g2d) {
+        int GRID_SIZE = board.getSize();
+
+        for(int i=0; i<GRID_SIZE; i++){
+            for(int j=0; j<GRID_SIZE; j++){
+                int number = board.getNum(i, j);
+                Color color = getTileColor(number);
+                g2d.setColor(color);
+
+                int x = j*(TILE_SIZE + MARGIN) + MARGIN;
+                int y = i*(TILE_SIZE + MARGIN) + MARGIN;
+
+                g2d.fillRoundRect(x, y, TILE_SIZE, TILE_SIZE, 10, 10);
+
+                if(number != 0) drawNumber(g2d, x, y, number);
+            }
+        }
+    }
+
+    private void drawNumber(Graphics2D g2d, int x, int y, int number) {
+        if(number <= 4){
+            g2d.setColor(new Color(119, 110, 101));
+        }else{
+            g2d.setColor(new Color(249, 246, 242));
+        }
+        g2d.setFont(new Font("Arial", Font.BOLD, 36));
+        String numberString = Integer.toString(number);
+        FontMetrics fm = g2d.getFontMetrics();
+
+        int numberX = x + (TILE_SIZE - fm.stringWidth(numberString)) / 2;
+        int numberY = y + (TILE_SIZE - fm.getHeight())/2 + fm.getAscent();
+
+        g2d.drawString(numberString, numberX, numberY);
+    }
+
+    public static Color getTileColor(int number) {
+        return switch (number) {
+            case 0 -> new Color(205, 193, 180);
+            case 2 -> new Color(238, 228, 218);
+            case 4 -> new Color(237, 224, 200);
+            case 8 -> new Color(242, 177, 121);
+            case 16 -> new Color(245, 149, 99);
+            case 32 -> new Color(246, 124, 95);
+            case 64 -> new Color(246, 94, 59);
+            case 128 -> new Color(237, 207, 114);
+            case 256 -> new Color(237, 204, 97);
+            case 512 -> new Color(237, 200, 80);
+            case 1024 -> new Color(237, 197, 63);
+            case 2048 -> new Color(237, 194, 46);
+            default -> new Color(60, 58, 50);
+        };
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        GridPanel panel = new GridPanel();
+        GameBoard gameBoard = new GameBoard();
+        BoardPanel panel = new BoardPanel(gameBoard);
 
         frame.add(panel);
         frame.pack();
