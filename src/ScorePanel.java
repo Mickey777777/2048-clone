615296283bc9597b933649e7a7f0b5175dd9fe04
Mickey777777 <wh879642@gmail.com>
2025-11-22 15:07:05 +0900

    feat: score, best 그리기

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -23,13 +23,13 @@ public class ScorePanel extends JPanel {
         int scoreX = PANEL_WIDTH / 2 - BOX_WIDTH / 2 - BOX_MARGIN;
         int scoreY = PANEL_HEIGHT / 2 - BOX_HEIGHT / 2;
         drawScoreBox(scoreX, scoreY, g2d, new Color(233, 231, 218));
-
+        drawScore(scoreX, scoreY, g2d, 100, "SCORE");
         // best
         int bestX = PANEL_WIDTH / 2 - BOX_WIDTH / 2 + BOX_MARGIN;
         int bestY = PANEL_HEIGHT / 2 - BOX_HEIGHT / 2;
         drawScoreBox(bestX, bestY, g2d, new Color(250, 248, 241));
         drawBoxBorder(bestX, bestY, g2d, new Color(233, 231, 218));
-
+        drawScore(bestX, bestY, g2d, 1000, "BEST");
     }
 
     private void drawScoreBox(int x, int y, Graphics2D g2d, Color color) {
@@ -41,5 +41,23 @@ public class ScorePanel extends JPanel {
         g2d.setColor(color);
         g2d.setStroke(new BasicStroke(2));
         g2d.drawRoundRect(x, y, BOX_WIDTH, BOX_HEIGHT, 20, 20);
+    }
+
+    private void drawScore(int x, int y, Graphics2D g2d, int score, String str) {
+        g2d.setColor(new Color(153, 139, 124));
+
+        g2d.setFont(new Font("Arial", Font.BOLD, 12));
+        FontMetrics fm = g2d.getFontMetrics();
+        int fontX = x + (BOX_WIDTH - fm.stringWidth(str)) / 2;
+        int fontY = y + (BOX_HEIGHT - fm.getHeight()) / 2 + fm.getAscent() - 10;
+        g2d.drawString(str, fontX, fontY);
+
+        g2d.setFont(new Font("Arial", Font.BOLD, 20));
+        String scoreString = Integer.toString(score);
+        fm = g2d.getFontMetrics();
+        fontX = x + (BOX_WIDTH - fm.stringWidth(scoreString)) / 2;
+        fontY = y + (BOX_HEIGHT - fm.getHeight()) / 2 + fm.getAscent() + 10;
+
+        g2d.drawString(scoreString, fontX, fontY);
     }
 }
